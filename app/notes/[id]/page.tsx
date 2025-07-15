73cd6d5792627431da64182c532377cabe1e72fd
VickTorry <victoriadudnyk@gmail.com>
2025-07-15 15:49:03 +0200

    changed Props to not await Promis

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -2,20 +2,15 @@ import { QueryClient, HydrationBoundary, dehydrate } from '@tanstack/react-query
 import { fetchNoteById } from '@/lib/api'
 import NoteDetailsClient from './NoteDetails.client' 
 
-
 type Props = {
-  params: Promise<{ id: string }>
+  params: { id: string }  // ✅ FIXED: params is not a Promise
 }
 
 export default async function NoteDetails({ params }: Props) {
-
-  const { id } = await params  // ✅ Await here to match what Turbopack expects
-  console.log('note id:', id);
-  const noteId = Number(id)
-  const note = await fetchNoteById(noteId)
-  console.log('Fetched note:', note) // ✅ Log the fetched note
+  const noteId = Number(params.id)
   const queryClient = new QueryClient()
 
+  // Preload the note
   await queryClient.prefetchQuery({
     queryKey: ['note', noteId],
     queryFn: () => fetchNoteById(noteId),
